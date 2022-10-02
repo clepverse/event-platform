18bf677620c73ae6e25771d9f7b68dc9ace9bf25
clepverse <cleepe5@hotmail.com>
2022-10-01 21:01:21 -0300

    Página Subscribe mutation graphql e melhorias na página Event

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { Event } from './pages/Event';
+import { Subscribe } from './pages/Subscribe';
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<div>HOME</div>} />
+      <Route path="/" element={<Subscribe />} />
       <Route path="/event" element={<Event />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.hygraph.com/v2/cl8gmbff94inp01ueevdwdu1i/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache({}),
 });
